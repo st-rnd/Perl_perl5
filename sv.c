ff36f84fa392cd79f1640080ee0b888f100a0890
Karl Williamson <khw@cpan.org>
2022-10-08 06:09:14 -0600

    newSVbool(): Fix compiler warning
    The parameter was declared const in embed.fnc, but not in the function
    prototype

--- sv.c
+++ sv.c
@@ -10021,7 +10021,7 @@ Creates a new SV boolean.
 */
 
 SV *
-Perl_newSVbool(pTHX_ bool bool_val)
+Perl_newSVbool(pTHX_ const bool bool_val)
 {
     PERL_ARGS_ASSERT_NEWSVBOOL;
     SV *sv = newSVsv(bool_val ? &PL_sv_yes : &PL_sv_no);
